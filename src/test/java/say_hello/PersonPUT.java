4bae27cfeb35e0316f915796b69753c84ae5a9c6
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2018-01-17 20:56:19 +0900

    exprerimented by d-morita

--- src/test/java/say_hello/PersonPUT.java
+++ src/test/java/say_hello/PersonPUT.java
@@ -10,11 +10,20 @@ import org.junit.runner.RunWith;
 public class PersonPUT {
     private static String   EXPECTED1 = "Hello, I am Ms. Alice.";
     private static Object[] INPUT1    = { Person.Sex.FEMALE, "Alice"};
+    private static String   EXPECTED2 = "Hello, I am Mr. Bob.";
+    private static Object[] INPUT2    = { Person.Sex.MALE, "Bob"};
+    private static String   EXPECTED3 = "Hello, I am Ms. Arisa.";
+    private static Object[] INPUT3    = { Person.Sex.FEMALE, "Arisa"};
+    private static String   EXPECTED4 = "Hello, I am Mr. Mike.";
+    private static Object[] INPUT4    = { Person.Sex.MALE, "Mike"};
 
 
     @DataPoints
     public static Fixture[] TestData = {
-        new Fixture(EXPECTED1, INPUT1), // Data1
+        new Fixture(EXPECTED1, INPUT1), // Data
+        new Fixture(EXPECTED2, INPUT2), // Data
+        new Fixture(EXPECTED3, INPUT3), // Data
+        new Fixture(EXPECTED4, INPUT4), // Data
     };
 
     @Theory
