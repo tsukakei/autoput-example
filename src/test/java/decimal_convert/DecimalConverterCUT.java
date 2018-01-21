b8407a920bb3046850a150a47cc9790992e05f41
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2018-01-21 10:18:57 +0900

    add k-kato's test cases

--- src/test/java/decimal_convert/DecimalConverterCUT.java
+++ src/test/java/decimal_convert/DecimalConverterCUT.java
@@ -5,8 +5,97 @@ import org.junit.Test;
 
 public class DecimalConverterCUT {
     @Test
-    public void testDecimalConvert() {
+    public void testDecimalConvert0() {
         String actual = DecimalConverter.decimalConvert(DecimalConverter.Binary, 2);
         Assert.assertEquals("10", actual);
+    }
+    // K.Kato
+    @Test
+    public void testDecimalConvert1() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Binary, 0);
+        Assert.assertEquals("0", actual);
+    }
+    @Test
+    public void testDecimalConvert2() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Binary, 10);
+        Assert.assertEquals("1010", actual);
+    }@Test
+    public void testDecimalConvert3() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Binary, 100);
+        Assert.assertEquals("1100100", actual);
+    }@Test
+    public void testDecimalConvert4() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Binary, Integer.MAX_VALUE);
+        Assert.assertEquals("1111111111111111111111111111111", actual);
+    }
+    @Test
+    public void testDecimalConvert5() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Octal, 0);
+        Assert.assertEquals("0", actual);
+    }
+    @Test
+    public void testDecimalConvert6() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Octal, 2);
+        Assert.assertEquals("2", actual);
+    }
+    @Test
+    public void testDecimalConvert7() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Octal, 10);
+        Assert.assertEquals("12", actual);
+    }
+    @Test
+    public void testDecimalConvert8() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Octal, 100);
+        Assert.assertEquals("144", actual);
+    }
+    @Test
+    public void testDecimalConvert9() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Decimal, 0);
+        Assert.assertEquals("0", actual);
+    }
+    @Test
+    public void testDecimalConvert10() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Decimal, 2);
+        Assert.assertEquals("2", actual);
+    }
+    @Test
+    public void testDecimalConvert11() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Decimal, 10);
+        Assert.assertEquals("10", actual);
+    }
+    @Test
+    public void testDecimalConvert12() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Decimal, 100);
+        Assert.assertEquals("100", actual);
+    }
+    @Test
+    public void testDecimalConvert13() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Hexadecimal, 0);
+        Assert.assertEquals("0", actual);
+    }
+    @Test
+    public void testDecimalConvert14() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Hexadecimal, 2);
+        Assert.assertEquals("2", actual);
+    }
+    @Test
+    public void testDecimalConvert15() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Hexadecimal, 10);
+        Assert.assertEquals("a", actual);
+    }
+    @Test
+    public void testDecimalConvert16() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Hexadecimal, 100);
+        Assert.assertEquals("64", actual);
+    }
+    @Test
+    public void testDecimalConvert17() {
+        String actual = DecimalConverter.decimalConvert(-1, 1);
+        Assert.assertEquals(null, actual);
+    }
+    @Test
+    public void testDecimalConvert18() {
+        String actual = DecimalConverter.decimalConvert(DecimalConverter.Binary, -1);
+        Assert.assertEquals(null, actual);
     }
 }
--- src/test/java/fizzbuzz/FizzBuzzCUT.java
+++ src/test/java/fizzbuzz/FizzBuzzCUT.java
@@ -9,6 +9,7 @@ public class FizzBuzzCUT {
         String actual = FizzBuzz.fizzbuzz(1);
         Assert.assertEquals("1", actual);
     }
+    // D.Morita
     @Test
     public void testFizzBuzz2() {
         String actual = FizzBuzz.fizzbuzz(3);
@@ -27,6 +28,82 @@ public class FizzBuzzCUT {
     @Test
     public void testFizzBuzz5() {
         String actual = FizzBuzz.fizzbuzz(-1);
+        Assert.assertEquals(null, actual);
+    }
+    // K.Kato
+    @Test
+    public void testFizzBuzz6() {
+        String actual = FizzBuzz.fizzbuzz(0);
+        Assert.assertEquals("FizzBuzz", actual);
+    }
+    @Test
+    public void testFizzBuzz7() {
+        String actual = FizzBuzz.fizzbuzz(2);
+        Assert.assertEquals("2", actual);
+    }
+    @Test
+    public void testFizzBuzz8() {
+        String actual = FizzBuzz.fizzbuzz(3);
+        Assert.assertEquals("Fizz", actual);
+    }
+    @Test
+    public void testFizzBuzz9() {
+        String actual = FizzBuzz.fizzbuzz(4);
+        Assert.assertEquals("4", actual);
+    }
+    @Test
+    public void testFizzBuzz10() {
+        String actual = FizzBuzz.fizzbuzz(5);
+        Assert.assertEquals("Buzz", actual);
+    }
+    @Test
+    public void testFizzBuzz11() {
+        String actual = FizzBuzz.fizzbuzz(6);
+        Assert.assertEquals("Fizz", actual);
+    }
+    @Test
+    public void testFizzBuzz12() {
+        String actual = FizzBuzz.fizzbuzz(10);
+        Assert.assertEquals("Buzz", actual);
+    }
+    @Test
+    public void testFizzBuzz13() {
+        String actual = FizzBuzz.fizzbuzz(15);
+        Assert.assertEquals("FizzBuzz", actual);
+    }
+    @Test
+    public void testFizzBuzz14() {
+        String actual = FizzBuzz.fizzbuzz(30);
+        Assert.assertEquals("FizzBuzz", actual);
+    }
+    @Test
+    public void testFizzBuzz15() {
+        String actual = FizzBuzz.fizzbuzz(-1);
+        Assert.assertEquals(null, actual);
+    }
+    @Test
+    public void testFizzBuzz16() {
+        String actual = FizzBuzz.fizzbuzz(-3);
+        Assert.assertEquals(null, actual);
+    }
+    @Test
+    public void testFizzBuzz17() {
+        String actual = FizzBuzz.fizzbuzz(-5);
+        Assert.assertEquals(null, actual);
+    }
+    @Test
+    public void testFizzBuzz18() {
+        String actual = FizzBuzz.fizzbuzz(-15);
+        Assert.assertEquals(null, actual);
+    }
+    @Test
+    public void testFizzBuzz19() {
+        String actual = FizzBuzz.fizzbuzz(Integer.MAX_VALUE);
+        Assert.assertEquals("2147483647", actual);
+    }
+    @Test
+    public void testFizzBuzz20() {
+        String actual = FizzBuzz.fizzbuzz(Integer.MIN_VALUE);
         Assert.assertEquals(null, actual);
     }
 }
