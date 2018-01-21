ffccb5a32c53aadea08b718642e96e65ab481f91
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2018-01-21 09:35:04 +0900

    Revert "static import"
    This reverts commit 884afe0009619039159c0bc91b571e566585c8ff.

--- src/test/java/triangle/TriangleCUT.java
+++ src/test/java/triangle/TriangleCUT.java
@@ -3,67 +3,61 @@ package triangle;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static triangle.Triangle.Equilateral;
-import static triangle.Triangle.IllegalInput;
-import static triangle.Triangle.Isosceles;
-import static triangle.Triangle.NotTriangle;
-import static triangle.Triangle.Scalene;
-
 public class TriangleCUT {
     @Test
     public void testTriangle1() {
         int ans = Triangle.judgeTriangle(0, 1, 1);
-        Assert.assertEquals(IllegalInput, ans);
+        Assert.assertEquals(Triangle.IllegalInput, ans);
     }
     @Test
     public void testTriangle2() {
         int ans = Triangle.judgeTriangle(1, 0, 1);
-        Assert.assertEquals(IllegalInput, ans);
+        Assert.assertEquals(Triangle.IllegalInput, ans);
     }
     @Test
     public void testTriangle3() {
         int ans = Triangle.judgeTriangle(1, 1, 0);
-        Assert.assertEquals(IllegalInput, ans);
+        Assert.assertEquals(Triangle.IllegalInput, ans);
     }
     @Test
     public void testTriangle4() {
         int ans = Triangle.judgeTriangle(2, 2, 3);
-        Assert.assertEquals(Isosceles, ans);
+        Assert.assertEquals(Triangle.Isosceles, ans);
     }
     @Test
     public void testTriangle5() {
         int ans = Triangle.judgeTriangle(2, 2, 1);
-        Assert.assertEquals(Isosceles, ans);
+        Assert.assertEquals(Triangle.Isosceles, ans);
     }
     @Test
     public void testTriangle6() {
         int ans = Triangle.judgeTriangle(3, 4, 5);
-        Assert.assertEquals(Scalene, ans);
+        Assert.assertEquals(Triangle.Scalene, ans);
     }
     @Test
     public void testTriangle7() {
         int ans = Triangle.judgeTriangle(2, 1, 1);
-        Assert.assertEquals(NotTriangle, ans);
+        Assert.assertEquals(Triangle.NotTriangle, ans);
     }
     @Test
     public void testTriangle8() {
         int ans = Triangle.judgeTriangle(0, 0, 0);
-        Assert.assertEquals(IllegalInput, ans);
+        Assert.assertEquals(Triangle.IllegalInput, ans);
     }
     @Test
     public void testTriangle9() {
         int ans = Triangle.judgeTriangle(5, 12, 13);
-        Assert.assertEquals(Scalene, ans);
+        Assert.assertEquals(Triangle.Scalene, ans);
     }
     @Test
     public void testTriangle10() {
         int ans = Triangle.judgeTriangle(5, 5, 5);
-        Assert.assertEquals(Equilateral, ans);
+        Assert.assertEquals(Triangle.Equilateral, ans);
     }
     @Test
     public void testTriangle11() {
         int ans = Triangle.judgeTriangle(5, 5, 8);
-        Assert.assertEquals(Isosceles, ans);
+        Assert.assertEquals(Triangle.Isosceles, ans);
     }
 
 
