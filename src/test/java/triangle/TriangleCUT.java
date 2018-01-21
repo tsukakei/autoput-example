4b49cde9046150224f394973f9a8bc92cbd3efa3
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2018-01-21 10:23:03 +0900

    add y-mao's test cases

--- src/test/java/triangle/TriangleCUT.java
+++ src/test/java/triangle/TriangleCUT.java
@@ -4,6 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TriangleCUT {
+
+    @Test
+    public void testTriangle0() {
+        Triangle.Type ans = Triangle.judgeTriangle(1, 1, 1);
+        Assert.assertEquals(Triangle.Type.Equilateral, ans);
+    }
+    // D.Morita
     @Test
     public void testTriangle1() {
         Triangle.Type ans = Triangle.judgeTriangle(0, 1, 1);
@@ -59,12 +66,45 @@ public class TriangleCUT {
         Triangle.Type ans = Triangle.judgeTriangle(5, 5, 8);
         Assert.assertEquals(Triangle.Type.Isosceles, ans);
     }
-
-
-
-
-
-
-
-
+    // Y.Mao
+    @Test
+    public void testTriangle12() {
+        Triangle.Type ans = Triangle.judgeTriangle(1, 1, 1);
+        Assert.assertEquals(Triangle.Type.Equilateral, ans);
+    }
+    @Test
+    public void testTriangle13() {
+        Triangle.Type ans = Triangle.judgeTriangle(5, 5, 6);
+        Assert.assertEquals(Triangle.Type.Isosceles, ans);
+    }
+    @Test
+    public void testTriangle14() {
+        Triangle.Type ans = Triangle.judgeTriangle(0, 1, 1);
+        Assert.assertEquals(Triangle.Type.IllegalInput, ans);
+    }
+    @Test
+    public void testTriangle15() {
+        Triangle.Type ans = Triangle.judgeTriangle(1, 10, 100);
+        Assert.assertEquals(Triangle.Type.NotTriangle, ans);
+    }
+    @Test
+    public void testTriangle16() {
+        Triangle.Type ans = Triangle.judgeTriangle(2, 3, 4);
+        Assert.assertEquals(Triangle.Type.Scalene, ans);
+    }
+    @Test
+    public void testTriangle17() {
+        Triangle.Type ans = Triangle.judgeTriangle(1, 0, 1);
+        Assert.assertEquals(Triangle.Type.IllegalInput, ans);
+    }
+    @Test
+    public void testTriangle18() {
+        Triangle.Type ans = Triangle.judgeTriangle(1, 1, 0);
+        Assert.assertEquals(Triangle.Type.IllegalInput, ans);
+    }
+    @Test
+    public void testTriangle19() {
+        Triangle.Type ans = Triangle.judgeTriangle(3, 4, 4);
+        Assert.assertEquals(Triangle.Type.Isosceles, ans);
+    }
 }
